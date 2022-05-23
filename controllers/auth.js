776179dcafac808f5d4f46aeae39910b3c568e68
Jesus Jo <jesusjho26@gmail.com>
2022-05-23 11:19:30 -0700

    login controller added

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,4 +25,35 @@ const registerCtrl = async (req, res) => {
     }
 }
 
-module.exports = {registerCtrl};
+//TODO this controller is used for login 
+const loginCtrl = async (req, res) => { 
+    try {
+        req = matchedData(req); 
+        const user = await userModel.findOne({email: req.email}).select("password name email"); 
+        if (!user) {
+            handleHttpError(res, 'USER_NOT_EXISTS', 404); 
+            return
+        }
+        const hashPassword = user.get('password'); 
+        const check = await compare(req.password, hashPassword); 
+        if (!check) {
+            handleHttpError(res, 'PASSWORD_INVALID', 401);
+            return
+        }
+        user.set('password', undefined, {strict: false}); 
+        const data = { 
+            token: await tokenSign(user), 
+            user
+        }; 
+        res.send({data}); 
+    } catch (error) {
+        handleHttpError(res, "ERROR_LOGIN_USER")
+    }
+
+
+}
+
+
+
+
+module.exports = {loginCtrl, registerCtrl};
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const { registerCtrl } = require('../controllers/auth'); 
+const { registerCtrl, loginCtrl } = require('../controllers/auth'); 
 const { validatorLogin, validatorRegister } = require('../validators/auth'); 
 const router = express.Router(); 
 
@@ -7,5 +7,7 @@ const router = express.Router();
 //TODO route to sign up new user localhost:3000/api/register
 router.post("/register", validatorRegister, registerCtrl); 
 
+//TODO route to sign in user 
+router.post('/login', validatorLogin, loginCtrl)
 
 module.exports = router;
